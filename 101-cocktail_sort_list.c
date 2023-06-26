1c16e9f7cb0f3e6f29d878807b06f6da5b6a7c3a
simonkari <kariithisimon1715@gmail.com>
2023-06-26 21:00:56 +0300

    101-cocktail_sort_list.c

--- 101-cocktail_sort_list.c
+++ 101-cocktail_sort_list.c
@@ -1,10 +1,11 @@
 #include "sort.h"
 
 /**
- * list_len - function returns length of list
- * @list: head of list
- * Return: length
- */
+ * list_len - Computes the length of a linked list.
+ * @list: The head of the linked list.
+ * Return: The length of the list.
+*/
+
 size_t list_len(listint_t *list)
 {
 	size_t len = 0;
@@ -14,14 +15,17 @@ size_t list_len(listint_t *list)
 		len++;
 		list = list->next;
 	}
+
 	return (len);
 }
 
 /**
- * switch_nodes - function swaps nodes at pointer p with the following node
- * @list: head of list
- * @p: pointer to node
- */
+ * switch_nodes - Swaps a node at the given pointer
+ * with the following node in a linked list.
+ * @list: The head of the linked list.
+ * @p: Pointer to the node to be swapped.
+*/
+
 void switch_nodes(listint_t **list, listint_t **p)
 {
 	listint_t *one, *two, *three, *four;
@@ -31,12 +35,15 @@ void switch_nodes(listint_t **list, listint_t **p)
 	three = (*p)->next;
 	four = (*p)->next->next;
 	two->next = four;
+
 	if (four)
 		four->prev = two;
 	three->next = two;
 	three->prev = two->prev;
+
 	if (one)
 		one->next = three;
+
 	else
 		*list = three;
 	two->prev = three;
@@ -44,10 +51,11 @@ void switch_nodes(listint_t **list, listint_t **p)
 }
 
 /**
- *  cocktail_sort_list - function sorts a doubly linked list using
- * the cocktail sort algorithm
- * @list: pointer to list
+ * cocktail_sort_list - Sorts a doubly linked list
+ * using the cocktail sort algorithm.
+ * @list: Pointer to the head of the list.
  */
+
 void cocktail_sort_list(listint_t **list)
 {
 	listint_t *p;
@@ -56,6 +64,7 @@ void cocktail_sort_list(listint_t **list)
 	if (!list || !*list || list_len(*list) < 2)
 		return;
 	p = *list;
+
 	while (!sorted)
 	{
 		sorted = 1;
@@ -70,9 +79,11 @@ void cocktail_sort_list(listint_t **list)
 			else
 				p = p->next;
 		}
+
 		if (sorted)
 			break;
 		p = p->prev;
+
 		while (p->prev)
 		{
 			if (p->n < p->prev->n)
@@ -82,6 +93,7 @@ void cocktail_sort_list(listint_t **list)
 				switch_nodes(list, &p);
 				print_list(*list);
 			}
+
 			else
 				p = p->prev;
 		}
